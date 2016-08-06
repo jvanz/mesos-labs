8b3532930bd3d3e0c95649091d0465bc96515c86
José Guilherme Vanz <guilherme.sft@gmail.com>
2016-08-06 12:34:18 -0300

    http_framework.py: accept resource offers
    The dummy HTTP framework has been uptated to accept rosource offers and
    launch dummy task. The task is only a "sleep 30" command.

--- http_framework.py
+++ http_framework.py
@@ -1,3 +1,8 @@
+"""
+    *** This is a dummy framework ***
+    This framework is implemented only for test a change in the Apache Mesos.
+"""
+
 import requests, json
 
 mesos_url = "http://localhost:5050/api/v1/scheduler"
@@ -8,6 +13,7 @@ class Framework:
         self.name = name
         self.id = None
         self.subscribed = False
+        self.task_counter = 0
 
     def run(self):
         subscribe_data = {"type": "SUBSCRIBE","subscribe":{"framework_info": {"user": "jvanz","name": self.name},"force": True}}
@@ -19,23 +25,47 @@ class Framework:
 
     def process(self,event):
         etype = event["type"]
-        print("event: ", event)
-        payload = { "framework_id" : {"value":self.id}}
+        print("event: ", etype)
         if "SUBSCRIBED" == etype:
             self.id = event["subscribed"]["framework_id"]["value"]
             self.subscribed= True
-        elif "HEARTBEAT" == etype:
-            pass
         elif "OFFERS" == etype:
-            payload["type"] = "DECLINE"
-            payload["decline"] = { "offer_ids" : []}
-            for offer in event["offers"]["offers"]:
-                payload["decline"]["offer_ids"].append({ "value": offer["id"]["value"]})
-            response = requests.post(mesos_url, headers=header, json=payload)
-            if response.status_code != 202:
-                print("Can not decline offers")
-            else:
-                print("The offers have been declined")
+            self.launch_task(event)
+        elif "UPDATE" == etype:
+            self.ack(event)
+
+    def launch_task(self, event):
+        accept = {"offer_ids": [], "operations": []}
+        for offer in event["offers"]["offers"]:
+            accept["offer_ids"].append({"value":offer["id"]["value"]})
+            mem_offer = offer["resources"][1]
+            cpu = {"name": "cpus", "type": "SCALAR", "scalar":{"value": 0.5}}
+            mem = {"name": "mem", "type": "SCALAR", "scalar": {"value": 50}}
+            task = {"name": "task-" + str(self.task_counter), "task_id": {"value":str(self.task_counter)},
+                    "agent_id": {"value":offer["agent_id"]["value"]}, "resources": [cpu,mem],
+                    "command": {"value": "sleep 30", "shell": True}}
+            accept["operations"].append({"type": "LAUNCH", "launch": {"task_infos": [task]}})
+            self.task_counter += 1
+        payload = {"type": "ACCEPT", "framework_id": {"value":self.id }, "accept": accept}
+        response = requests.post(mesos_url, headers=header, json=payload)
+        if response.status_code == 202:
+            print("Task launched successfully")
+        else:
+            print("Task launch failed")
+
+    def ack(self, event):
+        payload = {"type": "ACKNOWLEDGE", "framework_id": {"value":self.id },
+                "acknowledge": {
+                    "agent_id": event["update"]["status"]["agent_id"],
+                    "task_id": event["update"]["status"]["task_id"],
+                    "uuid": event["update"]["status"]["uuid"]
+                    }}
+        response = requests.post(mesos_url, headers=header, json=payload)
+        if response.status_code == 202:
+            print("ACK OK")
+        else:
+            print("ACK ** NOT ** OK")
+
 
 def main():
     framework = Framework("Dummy HTTP framework")
